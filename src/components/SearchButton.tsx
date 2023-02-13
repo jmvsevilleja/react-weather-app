7a6580f8a130657e6c68154a38a25fc32e166f1e
Jess Mark Sevilleja <jess@clinica.dev>
2023-02-13 11:41:27 +0800

    added initial search and button field

---
+++ src/components/SearchButton.tsx
@@ -0,0 +1,9 @@
+const SearchButton = () => {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Display Weather
+    </button>
+  );
+};
+
+export default SearchButton;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,22 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import SearchField from "../components/SearchField";
+import SearchButton from "../components/SearchButton";
 
 const HomePage = () => {
-  const { user } = useAuth0();
-  console.log("user", user);
+  const { user, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
   return (
     <div>
       {user?.picture} - {user?.name} - {user?.email} -{" "}
       {`https://github.com/${user?.nickname}`}
+      <div>
+        <SearchField />
+        <SearchButton />
+      </div>
     </div>
   );
 };
