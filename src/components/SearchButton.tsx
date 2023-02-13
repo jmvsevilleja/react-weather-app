8986fec7fa172e637ec011eca48ac510e4c6f66e
Jess Mark Sevilleja <jess@clinica.dev>
2023-02-13 17:14:39 +0800

    code cleanup

--- src/components/SearchButton.tsx
+++
@@ -1,9 +0,0 @@
-const SearchButton = () => {
-  return (
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      Display Weather
-    </button>
-  );
-};
-
-export default SearchButton;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,7 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from "../components/LoginButton";
 import { useNavigate } from "react-router-dom";
 
-const LoginPage = () => {
+const LoginPage: React.FC = () => {
   const navigate = useNavigate();
   const { isAuthenticated, isLoading } = useAuth0();
 
@@ -19,27 +19,17 @@ const LoginPage = () => {
   }
 
   return (
-    <div>
-      <div className="flex items-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            fillRule="evenodd"
-            d="M4.5 9.75a6 6 0 0111.573-2.226 3.75 3.75 0 014.133 4.303A4.5 4.5 0 0118 20.25H6.75a5.25 5.25 0 01-2.23-10.004 6.072 6.072 0 01-.02-.496z"
-            clipRule="evenodd"
-          />
-        </svg>
-        Weather Forcast
+    <div className="md:m-40 xl:mx-96 m-10">
+      <div className="flex flex-col justify-center space-y-4 ">
+        <div className="">
+          Welcome to the weather forecast web application. Please login with
+          your Github user to use the application and view the weather in your
+          city.
+        </div>
+        <div className="flex justify-start">
+          <LoginButton />
+        </div>
       </div>
-      <p>
-        Welcome to the weather forecast web application. Please login with your
-        Github user to use the application and view the weather in your city.
-      </p>
-      <LoginButton />
     </div>
   );
 };
